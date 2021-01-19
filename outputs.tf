e5a0ff65b54642ef477e42eb9a2a0b8815579ef8
Dave Farinelli <d@dfar.io>
2021-01-19 09:50:56 -0500

    Added IP adress output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "fqdn" {
     description = "Fully-qualified domain for App Gateway Public IP. Requires domain_name_label to be defined."
     value = azurerm_public_ip.publicip.fqdn
 }
+
+output "ip_address" {
+    description = "Public IP Address of App Gateway."
+    value = azurerm_public_ip.publicip.ip_address
+}
